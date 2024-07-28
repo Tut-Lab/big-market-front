9d0f43571b4b719a32bc0aa42dd4a6067d593729
zhengshuaijie <352326430@qq.com>
2024-07-28 13:25:38 +0800

    开发运维 第3节：第二阶段完成抽奖

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,13 +1,12 @@
-"use client";
 // 请求地址
 const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://console-mock.apipost.cn/mock/6afa907d-6678-45e2-b867-032a11090abd";
 
 /**
  * 装配抽奖
- * @param strategyId
+ * @param activityId
  */
-export const strategyArmory = (strategyId?: number) => {
-    return fetch(`${apiHostUrl}/api/v1/raffle/strategy/strategy_armory?strategyId=${strategyId}`, {
+export const activityStrategyArmory = (activityId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/activity/armory?activityId=${activityId}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -17,9 +16,10 @@ export const strategyArmory = (strategyId?: number) => {
 
 /**
  * 查询抽奖奖品列表
- * @param strategyId 策略ID
+ * @param userId 用户ID
+ * @param activityId 用户ID
  */
-export const queryRaffleAwardList = (strategyId?: number) => {
+export const queryRaffleAwardList = (userId?: string, activityId?: number) => {
     try {
         return fetch(`${apiHostUrl}/api/v1/raffle/strategy/query_raffle_award_list`, {
             method: 'POST',
@@ -27,7 +27,8 @@ export const queryRaffleAwardList = (strategyId?: number) => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                strategyId: strategyId
+                userId: userId,
+                activityId: activityId
             })
         });
     } catch (error) {
@@ -40,28 +41,20 @@ export const queryRaffleAwardList = (strategyId?: number) => {
 }
 
 /**
- * 随机抽奖接口
- * @param strategyId 策略ID
- *
- * {
- * 	"code": "0000",
- * 	"info": "调用成功",
- * 	"data": {
- * 	    "awardIndex": 1, // awardIndex 获得的是列表中第几个奖品，方便测试使用
- * 		"awardId": 535,
- * 		"awardTitle": "一部手机"
- * 	}
- * }
+ * 抽奖接口
+ * @param userId 用户ID
+ * @param activityId 用户ID
  */
-export const randomRaffle = (strategyId?: number) => {
+export const draw = (userId?: string, activityId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/strategy/random_raffle`, {
+        return fetch(`${apiHostUrl}/api/v1/raffle/activity/draw`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify({
-                strategyId: strategyId
+                userId: userId,
+                activityId: activityId
             })
         })
     } catch (error) {
